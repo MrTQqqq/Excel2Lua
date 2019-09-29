1a450e37b8d50523928b1a197ee6dd2945d4dd97
MrTQqqq <40017748+MrTQqqq@users.noreply.github.com>
2019-09-29 17:17:17 +0800

    Update checkin.py

--- conv/checkin.py
+++ conv/checkin.py
@@ -1,1 +1,20 @@
 
+def process(data):
+	sheet1 = data[0]
+	sheet2 = data[1]
+
+	res = {}
+	res['seven_day'] = {}
+	for info in sheet1:
+		res['seven_day'][info['days']] = info
+
+
+	res['total'] = {}
+
+	for info in sheet2:
+		res['total'][info['day']] = info
+
+
+	return res
+
+
